09e6f6b125ca7bf32b1df5d0d492e5f69319b0ad
Anthoxoo <antoine.guenot31450@gmail.com>
2026-07-06 15:03:27 +0200

    08 done

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,16 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Echo(string) => self.message = string,
+            Message::Move(point) => self.position = point,
+            Message::ChangeColor(red, green, blue) => self.color = (red, green, blue),
+            Message::Quit => self.quit = true,
+            Message::Resize { width, height } => {
+                self.width = width;
+                self.height = height
+            }
+        }
     }
 }
 
